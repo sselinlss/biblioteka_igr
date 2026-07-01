8b2c895bdb50aa9ce13bb129f5ab6a3281a607d8
sselinlss <leeeliiisss@gmail.com>
2026-07-01 15:49:28 +0300

    Добавления файла main для запуска приложения

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QFont
+from ui_main import MainWindow
+
+
+def main():
+    app = QApplication(sys.argv)
+    app.setApplicationName("Библиотека видеоигр")
+    app.setFont(QFont("Segoe UI", 10))
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
